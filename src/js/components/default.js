eb287f57c140f6f5e4f50f1aea004eff932b38cc
lingduzero <bz63@njit.edu>
2017-12-09 17:21:53 -0500

    footer and header

---
+++ src/js/components/default.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default class defaultClass extends React.Component {
+  render(){
+    return (
+
+    );
+  }
+}
--- src/js/root.js
+++ src/js/root.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './index';
-import ComponentList from './components/list';
-import ComponentDetails from './components/details';
 import {Router,Route,hashHistory} from 'react-router';
+import PCIndex from './components/pc_index';
+import MobileIndex from './components/mobile_index';
+import 'antd/dist/antd.css';
+import MediaQuery from 'react-responsive';
 
 export default class Root extends React.Component{
   render(){
     return (
-      //这里替换了之前的 Index，变成了程序的入口
-      <Router history={hashHistory}>
 
-        <Route component={Index} path="/">
-          <Route component={ComponentDetails} path="details"></Route>
-        </Route>
-
-        <Route component={ComponentList} path="list/:id"></Route>
-
-      </Router>
+      <div>
+        <MediaQuery query = '(min-device-width: 1224px)'>
+            <PCIndex/>
+        </MediaQuery>
+        <MediaQuery query = '(max-device-width: 1224px)'>
+            <MobileIndex/>
+        </MediaQuery>
+      </div>
     );
   };
 }
 
-ReactDOM.render(<Root/>, document.getElementById('example'));
+ReactDOM.render(<Root/>, document.getElementById('mainContainer'));
